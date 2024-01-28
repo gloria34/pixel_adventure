83b5d27f2cb64f1f8b97cfae4ec3c82fb419f6dd
gloria34 <galkina@dlit.dp.ua>
2024-01-28 18:57:17 +0200

    Add running animation

--- lib/actors/palyer.dart
+++ lib/actors/palyer.dart
@@ -7,7 +7,11 @@ enum PlayerState { idle, running }
 
 class Player extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure> {
+  String character;
+  Player({position, required this.character}):super(position: position);
+
   late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
 
   @override
@@ -17,10 +21,20 @@ class Player extends SpriteAnimationGroupComponent
   }
 
   void _onLoadAllAnimations() {
-    idleAnimation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+    };
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount, ) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/$character/$state (32x32).png'),
       SpriteAnimationData.sequenced(
-        amount: 11,
+        amount: amount,
         stepTime: stepTime,
         textureSize: Vector2.all(32),
       ),
